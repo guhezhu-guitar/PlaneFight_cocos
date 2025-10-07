1a951689c564595bff215e763f20e1c904e579cb
guhezhu-guitar <3095677916@qq.com>
2025-10-07 14:54:15 +0800

    新增碰撞事件

--- assets/Scripts/Enemy.ts
+++ assets/Scripts/Enemy.ts
@@ -1,4 +1,4 @@
-import { _decorator, Animation, animation, Component, Node } from 'cc';
+import { _decorator, Animation, animation, Collider2D, Component, Contact2DType, Node } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Enemy')
@@ -8,7 +8,14 @@ export class Enemy extends Component {
     @property(Animation)
     anim:Animation =null;
     start() {
-        this.anim.play();
+        //播放动画
+        // this.anim.play();
+
+        //注册单个碰撞体的回调函数
+        let collider = this.getComponent(Collider2D);
+        if(collider){
+            collider.on(Contact2DType.BEGIN_CONTACT,this.onBeginContact,this);
+        }
     }
 
     update(deltaTime: number) {
